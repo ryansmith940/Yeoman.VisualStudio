e770270ab82cabb26041e3bc0bbb65d3e315af63
Ryan Smith <ryansmith940@gmail.com>
2015-10-15 22:15:55 -0500

    adding run commands

--- Yeoman.VisualStudio/tools/Yeoman.VisualStudio.psm1
+++ Yeoman.VisualStudio/tools/Yeoman.VisualStudio.psm1
@@ -18,16 +18,38 @@
 	}
 }
 
-function Get-CheckEnvironment
+function Prepare-Environment
 {
 	if (-Not (Get-CommandExists node))
 	{
-		Write-Host "Node not available"
+		Write-Error "\"node\" is not available.  You will need to install it from https://nodejs.org/ and/or add it to your PATH."
+		return $False
 	}
 
-	Write-Error "error"
-	Throw ""
-	Write-Host "more stuff"
+	if (-Not (Get-CommandExists npm))
+	{
+		Write-Error "\"npm\" is not available.  You will need to install it from https://nodejs.org/ and/or add it to your PATH."
+		return $False
+	}
+
+	if (-Not (Get-CommandExists yo))
+	{
+		Write-Warning "Yeoman is not installed.  Installing it now..."
+		Install-NpmModule "yo" $True
+	}
 }
 
-Export-ModuleMember Get-CheckEnvironment
+function Install-NpmModule($moduleName, $globally)
+{
+	$command = "npm install " + @{$true="-g ";$false=" "}[$globally] + $moduleName
+	Run-Command $command
+}
+
+function Run-Command($command)
+{
+	$cmd = "cmd"
+	$arguments = "\c " + $command
+	& $cmd $arguments -wait
+}
+
+Export-ModuleMember Prepare-Environment
